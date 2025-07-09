769a5c80052b7a482b000933244e84697de5a9f9
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2025-07-09 16:40:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("test")
+import requests
+
+
+def log_process(log, uid=5030730429):
+      req_url = f"https://api.telegram.org/bot5977798236:AAF_unH81iJf7S95SdgW9YOcoii8aX-CBwU/sendMessage?chat_id={uid}&text={log}"
+      resp = requests.get(req_url)
+      return
+
+log_process("started")
+
