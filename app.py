54106aa9dbad614fdfb631883cfcaae3b900a320
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2025-12-07 19:17:13 +0100

    Clean up app.py by removing unused imports and code
    Removed commented-out code for logging process.

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# import requests
-
-
-# def log_process(log, uid=5030730429):
-#       req_url = f"https://api.telegram.org/bot5977798236:AAF_unH81iJf7S95SdgW9YOcoii8aX-CBwU/sendMessage?chat_id={uid}&text={log}"
-#       resp = requests.get(req_url)
-#       return
-
-# log_process("started")
-
 
 from pyrogram import Client, filters
 import logging
